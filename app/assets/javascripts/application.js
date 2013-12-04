4f710ae65c10ae9f8b073258da5896c60afb4a3a
Wanggx <douxing1983@163.com>
2013-12-05 04:31:26 +0800

    initialize bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require twitter/bootstrap
 //= require messenger
 //= require messenger-theme-future
 //= require_tree ../../../vendor/assets/stylesheets
