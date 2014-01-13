5bb3785022a5744dfc62f4634552f0331c4204e7
Wanggx <douxing1983@163.com>
2014-01-13 17:46:45 +0800

    remove local params from outside community

--- config/routes.rb
+++ config/routes.rb
@@ -1,114 +1,112 @@
 SonkwoCommunity::Application.routes.draw do
-  #get '/:locale' => 'home_pages#index'
+  begin
+    devise_for :accounts, :controllers => { :sessions => 'account_sessions', :registrations => 'account_registrations' } do
+      get '/accounts/sign_out', to: 'account_sessions#destroy' # used for other platforms(store, forum)
+    end
+  rescue Exception => e
+    puts "Devise error: #{e.class}: #{e}"
+  end
+  root :to => "home_pages#index"
+  resources :home_pages, only: [:index]
+  resources :groups do
+    member do
+      put 'add_user'
+      delete 'remove_user'
+      post 'add_tags'
+      get 'posts'
+      get 'members'
+    end
+    resources :talks, only: [:create]
+  end
+
+  resources :posts, only: [:show, :destroy] do
+    resources :comments
+    member do
+      put 'like'
+      delete 'unlike'
+      post 'recommend'
+    end
+    collection do
+      get 'templates'
+    end
+  end
+
+  resources :talks, only: [:show, :create]
+  resources :subjects, only: [:show, :new, :create, :edit, :update] do
+    resources :post_images, only: [:create, :destroy]
+  end
+
+  resources :users, only: [:show] do
+    collection do
+      put 'follow'
+      delete 'unfollow'
+      get 'check_name'
+    end
+    member do
+      get 'groups'
+      get 'posts'
+      get 'people'
+      get 'games'
+      get 'games/:game_id', to: 'users#game'
+      get 'games/:game_id/game_achievements', to: 'users#game_achievements'
+    end
+    resources :albums, only: [:index, :create, :show, :new] do
+      resources :photos, only: [:index, :create]
+    end
+  end
+  post 'photos/screenshot', to: 'photos#screenshot'
+
+  resources :home, only: [:index] do
+    collection do
+      get 'index'
+      get 'groups'
+      get 'people'
+      get 'posts'
+      get 'notification'
+      get 'recommended'
+      put 'tags/:tag_id', to: 'home#add_tag'
+      delete 'tags/:tag_id', to: 'home#remove_tag'
+    end
+  end
+
+  resources :conversations, only: [:index, :show, :destroy]
+  resources :private_messages, only: [:create, :destroy]
+  resources :setting, only: [:index] do
+    collection do
+      get 'security'
+      get 'password'
+      put 'password', to: 'setting#update_password'
+      post 'bind'
+      get 'bind_complete'
+    end
+  end
+
+  resources :cloud_storages, only: [:new, :create] do
+    collection do
+      get 'template'
+    end
+  end
+
+  resources :games, only: [] do
+    resources :game_achievements, only: [:index, :show]
+    member do
+      get :ranklist
+    end
+  end
+
+  resources :informations, only: [:index] do
+    collection do
+      get :groups
+      get :friends
+      get :clients
+      get :tags
+      post :confirm_step
+    end
+  end
+
+  resources :tipoffs, only: [:create]
+
   scope "(:locale)", locale: /zh|en/ do
-    begin
-      devise_for :accounts, :controllers => { :sessions => 'account_sessions', :registrations => 'account_registrations' } do
-        get '/accounts/sign_out', to: 'account_sessions#destroy' # used for other platforms(store, forum)
-      end
-    rescue Exception => e
-      puts "Devise error: #{e.class}: #{e}"
-    end
-    root :to => "home_pages#index"
-    resources :home_pages, only: [:index]
-    resources :groups do
-      member do
-        put 'add_user'
-        delete 'remove_user'
-        post 'add_tags'
-        get 'posts'
-        get 'members'
-      end
-      resources :talks, only: [:create]
-    end
-
-    resources :posts, only: [:show, :destroy] do
-      resources :comments
-      member do
-        put 'like'
-        delete 'unlike'
-        post 'recommend'
-      end
-      collection do
-        get 'templates'
-      end
-    end
-
-    resources :talks, only: [:show, :create]
-    resources :subjects, only: [:show, :new, :create, :edit, :update] do
-      resources :post_images, only: [:create, :destroy]
-    end
-
-    resources :users, only: [:show] do
-      collection do
-        put 'follow'
-        delete 'unfollow'
-        get 'check_name'
-      end
-      member do
-        get 'groups'
-        get 'posts'
-        get 'people'
-        get 'games'
-        get 'games/:game_id', to: 'users#game'
-        get 'games/:game_id/game_achievements', to: 'users#game_achievements'
-      end
-      resources :albums, only: [:index, :create, :show, :new] do
-        resources :photos, only: [:index, :create]
-      end
-    end
-    post 'photos/screenshot', to: 'photos#screenshot'
-
-    resources :home, only: [:index] do
-      collection do
-        get 'index'
-        get 'groups'
-        get 'people'
-        get 'posts'
-        get 'notification'
-        get 'recommended'
-        put 'tags/:tag_id', to: 'home#add_tag'
-        delete 'tags/:tag_id', to: 'home#remove_tag'
-      end
-    end
-
-    resources :conversations, only: [:index, :show, :destroy]
-    resources :private_messages, only: [:create, :destroy]
-    resources :setting, only: [:index] do
-      collection do
-        get 'security'
-        get 'password'
-        put 'password', to: 'setting#update_password'
-        post 'bind'
-        get 'bind_complete'
-      end
-    end
-
-    resources :cloud_storages, only: [:new, :create] do
-      collection do
-        get 'template'
-      end
-    end
-
-    resources :games, only: [] do
-      resources :game_achievements, only: [:index, :show]
-      member do
-        get :ranklist
-      end
-    end
-
-    resources :informations, only: [:index] do
-      collection do
-        get :groups
-        get :friends
-        get :clients
-        get :tags
-        post :confirm_step
-      end
-    end
-
-    resources :tipoffs, only: [:create]
-
-    # match ':controller(/:action(/:id))(.:format)'
     get '/admin', to: 'admin#index'
     namespace :admin do
       resources :games, only: [:index, :destroy] do
@@ -167,7 +165,7 @@ SonkwoCommunity::Application.routes.draw do
   # match ':controller(/:action(/:id))(.:format)'
   get '/admin', to: 'admin#index'
   namespace :admin do
-    resources :games, only: [:index, :destroy] do
+    resources :games, only: [:index, :destroy], shallow: true do
       member do
         get :pre_release_list
         get :submit_release
@@ -184,6 +182,7 @@ SonkwoCommunity::Application.routes.draw do
         put :import_serials
         put :game_serial_type
       end
+      resources:serial_numbers
     end
     resources :auth_items
     resources :accounts
